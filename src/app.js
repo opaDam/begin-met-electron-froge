967c0838c226ddac2e3ad48551832086cb10d10a
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-02 19:51:14 +0100

    update

--- src/app.js
+++ src/app.js
@@ -3,22 +3,7 @@ const fs = require("fs");
 const path = require("path");
 // ///////////////////////////////////////////////////////
 let dir_path = `${__dirname}/../muziek/`; // dir_path = MUZIEK MAP PATH
-// let dir_path = `${__dirname}/../mp3/`; // dir_path = MUZIEK MAP PATH
 let array_audio = [];
-// let array_audio2 = [];
-// let array_base64 = [];
-// let array_search = [];
-// let array_search2 = [];
-// let array_local = [];
-// let array = [];
-// let x = 1,
-//   index = 0,
-//   songPlay,
-//   blue_dump0,
-//   blue_dump1,
-//   artist0,
-//   title0,
-//   num;
 // ///////////////////////////////////////////////////////
 
 // // maak ARRAYs //////////////////////////////////////
@@ -34,31 +19,27 @@ function read_dir() {
         }
         if (path.extname(file) === ".mp3") {
           file = file.substring(0, file.length - 4);
-          array_audio.push(file); // ARRAY ////////
-        //   array_audio2.push(file);
+          array_audio.push(file); // ***** ARRAY 
         }
-        // console.log(index + " - " + file);
+        console.log(index + " - " + file);
       });
-
-    //   shuffle();
-      // array = array_audio2;
-      // read_tags();
     }
   });
 }
 // EINDE /////////////////////////////////////////////////
 read_dir();
 
-[
-    "America   Sister Golden Hair (with lyrics",
-    "Celine Dion - I m Alive",
-    "Mamita mía   Miguel Moly (Letra[1]",
-    "St Bernard Walt"
-]
+const print = document.querySelector('.prt_ul');
 
-[
-    "America   Sister Golden Hair (with lyrics",
-    "Celine Dion - I m Alive",
-    "Mamita mía   Miguel Moly (Letra[1]",
-    "St Bernard Walt"
-]
+array_audio.forEach((file, index) => {
+    print.innerHTML += `<li>${file}</li>`;
+})
+
+
+// [
+//     "America   Sister Golden Hair (with lyrics",
+//     "Celine Dion - I m Alive",
+//     "Mamita mía   Miguel Moly (Letra[1]",
+//     "St Bernard Walt"
+// ]
+
