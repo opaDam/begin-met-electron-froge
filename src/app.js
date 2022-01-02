04000dd1d912f743577ee76228174a06929feb74
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-02 18:00:30 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,37 +1,24 @@
-console.log('ok');
-// const { remote } = require("electron");
+// console.log('ok');
 const fs = require("fs");
 const path = require("path");
-let jsmediatags = require("jsmediatags");
-// const { on } = require("events");
-// ///////////////////////////////////////////////////////
-// const audioPlayer = document.getElementById("audioPlayer");
-// const playList = document.getElementById("playList");
-// const playList_blue = playList.getElementsByTagName("img");
-// const playList_li = playList.getElementsByTagName("li");
-// const title = document.getElementById("title");
-// const btnpause = document.getElementsByClassName("btn_pause");
-// const btnplay = document.getElementsByClassName("btn_play");
-// const input = document.getElementById("myInput");
-// const p_1 = document.getElementsByClassName("p_1");
 // ///////////////////////////////////////////////////////
 let dir_path = `${__dirname}/../muziek/`; // dir_path = MUZIEK MAP PATH
 // let dir_path = `${__dirname}/../mp3/`; // dir_path = MUZIEK MAP PATH
 let array_audio = [];
-let array_audio2 = [];
-let array_base64 = [];
-let array_search = [];
-let array_search2 = [];
-let array_local = [];
-let array = [];
-let x = 1,
-  index = 0,
-  songPlay,
-  blue_dump0,
-  blue_dump1,
-  artist0,
-  title0,
-  num;
+// let array_audio2 = [];
+// let array_base64 = [];
+// let array_search = [];
+// let array_search2 = [];
+// let array_local = [];
+// let array = [];
+// let x = 1,
+//   index = 0,
+//   songPlay,
+//   blue_dump0,
+//   blue_dump1,
+//   artist0,
+//   title0,
+//   num;
 // ///////////////////////////////////////////////////////
 
 // // maak ARRAYs //////////////////////////////////////
@@ -48,12 +35,12 @@ function read_dir() {
         if (path.extname(file) === ".mp3") {
           file = file.substring(0, file.length - 4);
           array_audio.push(file); // ARRAY ////////
-          array_audio2.push(file);
+        //   array_audio2.push(file);
         }
         // console.log(index + " - " + file);
       });
 
-      shuffle();
+    //   shuffle();
       // array = array_audio2;
       // read_tags();
     }
@@ -61,3 +48,17 @@ function read_dir() {
 }
 // EINDE /////////////////////////////////////////////////
 read_dir();
+
+[
+    "America   Sister Golden Hair (with lyrics",
+    "Celine Dion - I m Alive",
+    "Mamita mía   Miguel Moly (Letra[1]",
+    "St Bernard Walt"
+]
+
+[
+    "America   Sister Golden Hair (with lyrics",
+    "Celine Dion - I m Alive",
+    "Mamita mía   Miguel Moly (Letra[1]",
+    "St Bernard Walt"
+]
